b403441e7fa33746f31006aeb8a121d49d93548a
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-02-08 06:02:04 -0300

    routers

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.post('/polls', request => {
+  console.log(request.body)
+})
+
 app.listen({ port: 3333 }).then(() => {
   console.log('Hello Word')
 })
