3b38ee3eaa15ec8d3e58aeb2c9fc020e9d2a8f14
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-02-09 06:19:02 -0300

    create polls roter

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,29 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
 
 const app = fastify()
 
-app.post('/polls', request => {
-  console.log(request.body)
+const prisma = new PrismaClient()
+
+app.post('/polls', async request => {
+  const createPolls = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPolls.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  return poll
+})
+
+app.get('/polls', () => {
+  return prisma.poll.findMany()
 })
 
 app.listen({ port: 3333 }).then(() => {
